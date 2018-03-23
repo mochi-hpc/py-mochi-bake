9102d4a9b9cff07938dd98f4f0152e1e26346a14
Matthieu Dorier <mdorier@anl.gov>
2018-03-23 15:33:05 +0100

    renamed _bake_inst into _provider

--- pybake/server.py
+++ pybake/server.py
@@ -9,20 +9,20 @@ class BakeProvider(pymargo.Provider):
 
 	def __init__(self, mid, mplex_id):
 		super(BakeProvider, self).__init__(mid, mplex_id)
-		self._bake_inst = _pybakeserver.register(mid._mid, mplex_id)
+		self._provider = _pybakeserver.register(mid._mid, mplex_id)
 
 	def add_storage_target(self, name):
-		tid = _pybakeserver.add_storage_target(self._bake_inst, name)
+		tid = _pybakeserver.add_storage_target(self._provider, name)
 		return BakeStorageTarget(tid)
 
 	def remove_storage_target(self, target):
-		return _pybakeserver.remove_storage_target(self._bake_inst, target._tid)
+		return _pybakeserver.remove_storage_target(self._provider, target._tid)
 
 	def remove_all_storage_targets(self):
-		return _pybakeserver.remove_all_storage_targets(self._bake_inst)
+		return _pybakeserver.remove_all_storage_targets(self._provider)
 
 	def count_storage_targets(self):
-		return _pybakeserver.count_storage_targets(self._bake_inst)
+		return _pybakeserver.count_storage_targets(self._provider)
 
 	def list_storage_targets(self):
-		return _pybakeserver.list_storage_targets(self._bake_inst)
+		return _pybakeserver.list_storage_targets(self._provider)
